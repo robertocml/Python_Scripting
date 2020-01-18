4b75cf4de64ede7e3a914a338572d91f564ccbde
robertocml <roberto.perezb@outlook.com>
2020-01-17 22:32:50 -0600

    init

---
+++ Keras_basics.py
@@ -0,0 +1,15 @@
+import numpy as numpy
+from random import randint
+from sklearn.preprocessing import MinMaxScaler
+
+
+train_labels = []
+train_samples = []
+
+ #Datos para este caso
+
+'''
+Datos de ejemplo:
+
+
+'''

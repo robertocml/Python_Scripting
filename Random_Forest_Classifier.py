2a34d158998daccd99a9a1921d735144d90b9b08
robertocml <roberto.perezb@outlook.com>
2020-01-19 23:43:22 -0600

    finished

--- Random_Forest_Classifier.py
+++ Random_Forest_Classifier.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 np.random.seed(0)
 
 # Cargamos del dataset de ejemplo 
@@ -26,4 +27,20 @@ train,test = train_test_split(df, test_size=0.2)
 
 # Ahora hagamos una lista con los features para usarla mas adelante..
 features = df.columns[:4]
-print(features)
+
+#Ahora convertimos cada especia en un digito.. para que sea mas sencillo
+y = pd.factorize(train['species'])[0]
+y_test = pd.factorize(test['species'])[0]
+
+## Ahora si creamos el clasificador de Bosques aleatorios
+clf = RandomForestClassifier(n_jobs=2,random_state=0,n_estimators=10)
+## Ajustamos el modelo!
+clf.fit(train[features],y)
+
+# Ahora aplicamos nuestro clasificador entrenado a nuestro test set
+predictions = clf.predict(test[features])
+
+#Chequemos el accuracy de nuestro modelo
+accuracy = accuracy_score(y_test, predictions)
+
+print("Accuracy del modelo : ",accuracy)

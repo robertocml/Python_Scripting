a3a4ded6bcf9a2d0de5a8900941eb37659bf2557
robertocml <roberto.perezb@outlook.com>
2020-01-21 22:33:20 -0600

    init

--- Web_scraping_books.py
+++ Web_scraping_books.py
@@ -4,6 +4,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome
 import pandas as pd
 import time
-path = 'C:\Users\rober\Downloads\archives\chromedriver_win32'
+path = 'C:\\Users\\rober\Downloads\\archives\\chromedriver_win32\\chromedriver.exe'
 chrome_options = Options()
 chrome_options.add_argument("- incognito")
+browser = webdriver.Chrome(path,options=chrome_options)
+pages = 3
+url = 'http://books.toscrape.com/catalogue/page-1.html'
+
+def getdata(start_url,pgs):
+    current = 0
+    urls = browser.get(start_url)

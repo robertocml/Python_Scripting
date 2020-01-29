2bdcce1a2e61b215023e3b247ea664491a06ae7a
robertocml <roberto.perezb@outlook.com>
2020-01-28 23:57:16 -0600

    testing funcs

--- Web_scraping_books.py
+++ Web_scraping_books.py
@@ -1,21 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import Chrome2
-import pandas as pd
-import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver import Chrome2
+# import pandas as pd
+# import time
+import requests
+from bs4 import BeautifulSoup
 
-path = 'C:\\Users\\rober\Downloads\\archives\\chromedriver_win32\\chromedriver.exe'
-chrome_options = Options()
-chrome_options.add_argument("- incognito")
-browser = webdriver.Chrome(path, options=chrome_options)
-pages = 3
+# Version with Selenium......
+# path = 'C:\\Users\\rober\Downloads\\archives\\chromedriver_win32\\chromedriver.exe'
+# chrome_options = Options()
+# chrome_options.add_argument("- incognito")
+# browser = webdriver.Chrome(path, options=chrome_options)
+# pages = 3
+
+req = requests.get('http://books.toscrape.com/catalogue/page-1.html')
+soup = BeautifulSoup(req.text, "lxml")
+
 url = 'http://books.toscrape.com/catalogue/page-1.html'
 
-
-def getdata(start_url, pgs):
-    current = 0
-    urls = browser.get(start_url)
+for tag in soup.find_all('a'):
+    print(tag.text)
 
 
-print("Testing pycharm vcs.")
+body = soup.find("article", {'class':'product_prod'}).get_text()
+print(len(body))

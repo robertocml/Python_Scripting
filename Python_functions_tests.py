f710bce05e83e0433d9abfcfe1aef77d0f5ac12b
robertocml <roberto.perezb@outlook.com>
2020-03-05 21:52:08 -0600

    Implementación de una regresión logística simple.

--- Python_functions_tests.py
+++ Python_functions_tests.py
@@ -1,8 +1,11 @@
+# Easy Examples of zip function..
 number_list = [1, 2, 3]
 str_list = ['one', 'two', 'three']
 
 # No iterables are passed
+# This is a zip object (iterable)
 result = zip()
+print(type(result))
 
 # Converting itertor to list
 result_list = list(result)
@@ -12,5 +15,13 @@ print(result_list)
 result = zip(number_list, str_list)
 
 # Converting itertor to set
-result_set = set(result)
-print(result_set)
+result_list = list(result)
+
+# Check how you do NOT need it to convert it to a list
+# within the for loop to unpack the zip object, but if its
+# converted to a list also works..
+for one, two in list(zip(number_list, str_list)):
+    print(one, two)
+
+for one, two in zip(number_list, str_list):
+    print(one)
